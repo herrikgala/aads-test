b9a885ed6b0fdda71cc2582f7c13c84f75f26413
Nur Amandurdyev <m.amandurdyyev@bslab.io>
2025-07-08 17:29:32 +0500

    base path fixed for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/aads-test', // Updated to match the repository name
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
